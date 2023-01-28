2b8db68c209a3c8d6c0aa3d25eb8573f34d6de41
finnishguy1 <nokkapaa@gmail.com>
2023-01-28 15:39:35 +0200

    update tiff file

--- data_processing/tif_to_tensor.py
+++ data_processing/tif_to_tensor.py
@@ -1,2 +1,12 @@
 import tensorflow as tf
+import tensorflow_io as tfio
 
+path = r"C:\Users\robin\OneDrive\Työpöytä\Retinas\Retinal-Veinmapping\Dataset\training\images\21_training.tif"
+
+
+def parse_image(img_path: str) -> dict:
+    image = tf.io.read_file(img_path)
+    tfio.experimental.image.decode_tiff(image)
+
+
+parse_image(path)
