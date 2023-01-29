c192826e1d286ba3af3e158f691eac2868cb8509
Elias Simojoki <eliassimojoki0@gmail.com>
2023-01-30 00:48:35 +0200

    more to prac

--- data_processing/prac.py
+++ data_processing/prac.py
@@ -14,17 +14,18 @@ from IPython import display
 PATH = r"C:\Users\elias\ML\Retinal-Veinmapping\Dataset\training"
 
 
-
+path = pathlib.Path(PATH)
 
 
 
 def load(num): # returns tensor of image
 
-    real = tf.io.read_file(f"{path}\gifimages/{num}_training.jpg")
-    real = tf.io.decode_image(real, expand_animations=False)
+    real = tf.io.read_file(fr"{path}\jpgimages/{num}_training.jpg")
+    real = tf.io.decode_jpeg(real)
 
-    target = tf.io.read_file(fr"{path}\1st_manualjpg/{num}_manual1.jpg")
-    target = tf.io.decode_image(target, expand_animations=False)
+    target = tf.io.read_file(fr"{path}\1st_manualjpg/{num}_manual1-0000.jpg")
+    target = tf.io.decode_jpeg(target)
+    target = tf.image.grayscale_to_rgb(target,name=None)
 
     return tf.cast(real, tf.float32), tf.cast(target, tf.float32)
 
@@ -67,26 +68,55 @@ def normalize(input_image, real_image):
 @tf.function()
 def random_jitter(input_image, real_image):
   # Resizing to 286x286
-  input_image, real_image = resize(input_image, real_image, 286, 286)
+  input_image, real_image = resize(input_image, real_image, IMG_HEIGHT, IMG_WIDTH)
 
   # Random cropping back to 256x256
   input_image, real_image = random_crop(input_image, real_image)
+
+ 
 
   if tf.random.uniform(()) > 0.5:
     # Random mirroring
     input_image = tf.image.flip_left_right(input_image)
     real_image = tf.image.flip_left_right(real_image)
 
+  if tf.random.uniform(()) > 0.5:
+
+    input_image = tf.image.flip_up_down(input_image)
+    real_image = tf.image.flip_up_down(real_image)
+
   return input_image, real_image
 
 
 inp, re = load(21)
-print(inp.shape, re.shape)
 
-plt.figure(figsize=(6, 6))
-for i in range(4):
-  rj_inp, rj_re = random_jitter(inp, re)
-  plt.subplot(2, 2, i + 1)
-  plt.imshow(rj_inp / 255.0)
-  plt.axis('off')
-plt.show()
+
+
+def load_image_train(num):
+  input_image, real_image = load(num)
+  input_image, real_image = random_jitter(input_image, real_image)
+  input_image, real_image = normalize(input_image, real_image)
+
+  return input_image, real_image
+
+
+
+def load_image_test(image_file):
+  input_image = load(image_file)
+  input_image = resize(input_image,
+                                   IMG_HEIGHT, IMG_WIDTH)
+  input_image, _ = normalize(input_image, input_image)
+
+  return input_image
+
+
+
+
+
+train_dataset = tf.data.Dataset.from_tensor_slices(
+    [num for num in range(21, 41)])
+
+train_dataset = train_dataset.map(load_image_train,
+                                num_parallel_calls=tf.data.AUTOTUNE)
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.batch(BATCH_SIZE)
