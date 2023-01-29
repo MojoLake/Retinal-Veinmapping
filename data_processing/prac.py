45a3efad08a9fd835be5e84b84435aee226bed42
finnishguy1 <nokkapaa@gmail.com>
2023-01-30 00:14:36 +0200

    prac not rlly

--- data_processing/prac.py
+++ data_processing/prac.py
@@ -11,17 +11,28 @@ from matplotlib import pyplot as plt
 from IPython import display
 
 
-PATH = r"C:\Users\elias\ML\Retinal-Veinmapping\Dataset\training\gifimages"
+EPATH = r"C:\Users\elias\ML\Retinal-Veinmapping\Dataset\training\gifimages"
+RPATH = r"C:\Users\robin\OneDrive\Työpöytä\Retinas\Retinal-Veinmapping\Dataset\training\gifimages"
 
-path = pathlib.Path(PATH)
+epath = pathlib.Path(EPATH)
+rpath = pathlib.Path(RPATH)
 
-
-sample_image = tf.io.read_file(str(path / '21_training.gif'))
+sample_image = tf.io.read_file(str(rpath / '21_training.gif'))
 sample_image = tf.io.decode_image(sample_image, expand_animations=False)     
-
 
 
 def load(image_file):
 
     image = tf.io.read_file(image_file)
-    image = tf.io.decode_gif(image, expand_ainmiations=False)
+    image = tf.io.decode_image(image, expand_animations=False)
+    return(image)
+
+
+img = load(str(rpath / '21_training.gif'))
+
+plt.figure()
+plt.imshow(img)
+plt.show()
+
+
+
