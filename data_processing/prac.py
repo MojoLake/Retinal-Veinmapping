645f6075cff3ec338fd25f6c1a7141624634eef4
Elias Simojoki <eliassimojoki0@gmail.com>
2023-01-30 22:15:12 +0200

    somewhat working demo

--- data_processing/prac.py
+++ data_processing/prac.py
@@ -5,39 +5,41 @@ import pathlib
 import time
 import datetime
 
+from skimage import io
 from matplotlib import pyplot as plt
 
 
 from IPython import display
 
 
-PATH = r"C:\Users\elias\ML\Retinal-Veinmapping\Dataset\training"
+PATH = r"C:\Users\elias\ML\Retinal-Veinmapping\Dataset"
 
 
 path = pathlib.Path(PATH)
 
 
 
-def load(num): # returns tensor of image
+def load(image_file): # returns tensor of image
 
-    real = tf.io.read_file(fr"{path}\jpgimages/{num}_training.jpg")
-    real = tf.io.decode_jpeg(real)
+    # print("IMAGE FILE:", image_file)
+    image = tf.io.read_file(image_file)
+    image = tf.io.decode_jpeg(image)
+    image = tf.cast(image, tf.float32)
+    # plt.figure()
+    # plt.imshow(image / 255.0)
+    # plt.show()
 
-    target = tf.io.read_file(fr"{path}\1st_manualjpg/{num}_manual1-0000.jpg")
-    target = tf.io.decode_jpeg(target)
-    target = tf.image.grayscale_to_rgb(target,name=None)
 
-    return tf.cast(real, tf.float32), tf.cast(target, tf.float32)
+    return image
 
 
-print(load(21)[0].shape)
 
 BUFFER_SIZE = 20
 
 BATCH_SIZE = 1
 
-IMG_WIDTH = 565
-IMG_HEIGHT = 584
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
 
 
 
@@ -68,10 +70,10 @@ def normalize(input_image, real_image):
 @tf.function()
 def random_jitter(input_image, real_image):
   # Resizing to 286x286
-  input_image, real_image = resize(input_image, real_image, IMG_HEIGHT, IMG_WIDTH)
-
+#   input_image, real_image = resize(input_image, real_image, 286, 286)
+    # these twom make some of the picture blue for some reason
   # Random cropping back to 256x256
-  input_image, real_image = random_crop(input_image, real_image)
+#   input_image, real_image = random_crop(input_image, real_image)
 
  
 
@@ -88,44 +90,68 @@ def random_jitter(input_image, real_image):
   return input_image, real_image
 
 
-inp, re = load(21)
+inp = load(str(path / r'training\images_256\22_train.jpg'))
+re = load(str(path / r'training\1st_manual_256\22_manual.jpg'))
+# plt.figure()
+# plt.imshow(inp / 255.0)
+# plt.figure()
+# plt.imshow(re / 255.0)
+# plt.show()
 
 
 
-def load_image_train(num):
-  input_image, real_image = load(num)
-  input_image, real_image = random_jitter(input_image, real_image)
-  input_image, real_image = normalize(input_image, real_image)
 
-  return input_image, real_image
+def load_image_train(image_file):
+  input_image = load(image_file)
+
+  return input_image
+
+
+def create_more(input_image, real_image):
+    input_image, real_image = random_jitter(input_image, real_image)
+    input_image, real_image = normalize(input_image, real_image)
+    return input_image, real_image
 
 
 
 def load_image_test(image_file):
   input_image = load(image_file)
-  input_image = resize(input_image,
-                                   IMG_HEIGHT, IMG_WIDTH)
+  # input_image = resize(input_image,
+                                  #  IMG_HEIGHT, IMG_WIDTH)
   input_image, _ = normalize(input_image, input_image)
 
   return input_image
 
 
 
+real_list = [load(str(path / fr'training\images_256\{num}_train.jpg')) for num in range(21, 41)]
+target_list = [load(str(path / fr'training\1st_manual_256\{num}_manual.jpg')) for num in range(21, 41)]
 
+real_set = tf.data.Dataset.from_tensor_slices(real_list)
+target_set = tf.data.Dataset.from_tensor_slices(target_list)
 
-train_dataset = tf.data.Dataset.from_tensor_slices(
-    [num for num in range(21, 41)])
+# real_set = real_set.map(load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
+# target_set = target_set.map(load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
+                            
 
-train_dataset = train_dataset.map(load_image_train,
-                                num_parallel_calls=tf.data.AUTOTUNE)
+train_dataset = tf.data.Dataset.zip((real_set, target_set))
+train_dataset = train_dataset.map(create_more, 
+                                 num_parallel_calls=tf.data.AUTOTUNE)
+
 train_dataset = train_dataset.shuffle(BUFFER_SIZE)
 train_dataset = train_dataset.batch(BATCH_SIZE)
 
+
+test_list = [load_image_test(str(path / fr'test\jpg_image_256\{num}_test.jpg')) for num in range(1, 21)]
+
+test_dataset = tf.data.Dataset.from_tensor_slices(test_list)
+test_dataset = test_dataset.batch(BATCH_SIZE)
 
 
 
 
 OUTPUT_CHANNELS = 3
+
 
 
 def downsample(filters, size, apply_batchnorm=True):
@@ -144,10 +170,10 @@ def downsample(filters, size, apply_batchnorm=True):
   return result
 
 
-
 down_model = downsample(3, 4)
 down_result = down_model(tf.expand_dims(inp, 0))
 print (down_result.shape)
+
 
 
 
@@ -171,7 +197,6 @@ def upsample(filters, size, apply_dropout=False):
   return result
 
 
-
 up_model = upsample(3, 4)
 up_result = up_model(down_result)
 print (up_result.shape)
@@ -179,9 +204,8 @@ print (up_result.shape)
 
 
 
-
 def Generator():
-  inputs = tf.keras.layers.Input(shape=[584, 566, 3])
+  inputs = tf.keras.layers.Input(shape=[256, 256, 3])
 
   down_stack = [
     downsample(64, 4, apply_batchnorm=False),  # (batch_size, 128, 128, 64)
@@ -232,4 +256,196 @@ def Generator():
 
 
 generator = Generator()
-tf.keras.utils.plot_model(generator, show_shapes=True, dpi=64)
+print(inp)
+# tf.keras.utils.plot_model(generator, show_shapes=True, dpi=64)
+inp = inp[:,:,:3]
+print("Inp.shape:", inp.shape)
+gen_output = generator(inp[None,:,:,:], training=False)
+plt.imshow(gen_output[0, ...])
+plt.show()
+
+
+
+LAMBDA = 100
+
+
+loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+
+def generator_loss(disc_generated_output, gen_output, target):
+  gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
+
+  # Mean absolute error
+  l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+
+  total_gen_loss = gan_loss + (LAMBDA * l1_loss)
+
+  return total_gen_loss, gan_loss, l1_loss
+
+
+
+
+def Discriminator():
+  initializer = tf.random_normal_initializer(0., 0.02)
+ 
+  inp = tf.keras.layers.Input(shape=[256, 256, 3], name='input_image')
+  tar = tf.keras.layers.Input(shape=[256, 256, 3], name='target_image')
+
+  x = tf.keras.layers.concatenate([inp, tar])  # (batch_size, 256, 256, channels*2)
+
+  down1 = downsample(64, 4, False)(x)  # (batch_size, 128, 128, 64)
+  down2 = downsample(128, 4)(down1)  # (batch_size, 64, 64, 128)
+  down3 = downsample(256, 4)(down2)  # (batch_size, 32, 32, 256)
+
+  zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)  # (batch_size, 34, 34, 256)
+  conv = tf.keras.layers.Conv2D(512, 4, strides=1,
+                                kernel_initializer=initializer,
+                                use_bias=False)(zero_pad1)  # (batch_size, 31, 31, 512)
+
+  batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
+
+  leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
+
+  zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)  # (batch_size, 33, 33, 512)
+
+  last = tf.keras.layers.Conv2D(1, 4, strides=1,
+                                kernel_initializer=initializer)(zero_pad2)  # (batch_size, 30, 30, 1)
+
+  return tf.keras.Model(inputs=[inp, tar], outputs=last)
+
+
+
+
+discriminator = Discriminator()
+
+# print("Input shape:", (inp[None,:,:,:]).shape)
+# disc_out = discriminator([inp[None,:,:,:], gen_output], training=False)
+# plt.imshow(disc_out[0, ..., -1], vmin=-20, vmax=20, cmap='RdBu_r')
+# plt.colorbar()
+# plt.show()
+
+
+
+def discriminator_loss(disc_real_output, disc_generated_output):
+  real_loss = loss_object(tf.ones_like(disc_real_output), disc_real_output)
+
+  generated_loss = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
+
+  total_disc_loss = real_loss + generated_loss
+
+  return total_disc_loss
+
+
+
+
+generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+
+
+def generate_images(model, test_input):
+  prediction = model(test_input, training=True)
+  plt.figure(figsize=(15, 15))
+
+  display_list = [test_input[0], prediction[0]]
+  title = ['Input Image', 'Predicted Image']
+
+  for i in range(2):
+    plt.subplot(1, 3, i+1)
+    plt.title(title[i])
+    # Getting the pixel values in the [0, 1] range to plot.
+    plt.imshow(display_list[i] * 0.5 + 0.5)
+    plt.axis('off')
+  plt.show()
+
+
+
+# for example_input  in test_dataset.take(1):
+  # generate_images(generator, example_input)
+
+
+
+log_dir="logs/"
+
+summary_writer = tf.summary.create_file_writer(
+log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+
+
+
+@tf.function
+def train_step(input_image, target, step):
+  print("Train_step:", input_image.shape)
+  with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+    gen_output = generator(input_image, training=True)
+
+    disc_real_output = discriminator([input_image, target], training=True)
+    disc_generated_output = discriminator([input_image, gen_output], training=True)
+
+    gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_output, target)
+    disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+
+  generator_gradients = gen_tape.gradient(gen_total_loss,
+                                          generator.trainable_variables)
+  discriminator_gradients = disc_tape.gradient(disc_loss,
+                                               discriminator.trainable_variables)
+
+  generator_optimizer.apply_gradients(zip(generator_gradients,
+                                          generator.trainable_variables))
+  discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
+                                              discriminator.trainable_variables))
+
+  with summary_writer.as_default():
+    tf.summary.scalar('gen_total_loss', gen_total_loss, step=step//1000)
+    tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=step//1000)
+    tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=step//1000)
+    tf.summary.scalar('disc_loss', disc_loss, step=step//1000)
+
+
+
+
+  
+def fit(train_ds, test_ds, steps):
+  example_input = next(iter(test_ds.take(1)))
+  example_input = example_input[:,:,:,:3]
+  start = time.time()
+
+  for step, (input_image, target) in train_ds.repeat().take(steps).enumerate():
+    input_image = input_image[:,:,:,:3]
+    target = target[:,:,:,:3]
+    print(step)
+    if (step) % 200 == 0:
+      display.clear_output(wait=True)
+
+      if step != 0:
+        print(f'Time taken for 1000 steps: {time.time()-start:.2f} sec\n')
+
+      start = time.time()
+
+      generate_images(generator, example_input)
+      print(f"Step: {step//10}k")
+
+    train_step(input_image, target, step)
+    # inp[None,:,:,:]
+
+    # Training step
+    if (step+1) % 10 == 0:
+      print('.', end='', flush=True)
+
+
+    # Save (checkpoint) the model every 5k steps
+    if (step + 1) % 5 == 0:
+      checkpoint.save(file_prefix=checkpoint_prefix)
+
+
+
+fit(train_dataset, test_dataset, steps=40000)
+
