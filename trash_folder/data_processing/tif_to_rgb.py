e3049750152edf240e12066b1b68e8d08af66c89
Elias Simojoki <eliassimojoki0@gmail.com>
2023-02-01 16:48:34 +0200

    the model

---
+++ trash_folder/data_processing/tif_to_rgb.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from PIL import Image
+
+
+def get_rgb(dir):
+    files = Path(dir).glob('*')
+    ret = []
+
+    for file in files:
+        image = Image.open(file)
+        image.load()
+
+        cur = Image.new('RGB', image.size, (255, 255, 255))
+        cur.paste(image, None)
+        ret.append(cur)
+
+    return ret
+
+
+dir = r"C:\Users\elias\OneDrive\Työpöytä\Code\ml\Retinal-veinmapping\Dataset/test/images/"
+
+print(get_rgb(dir))
+
+
